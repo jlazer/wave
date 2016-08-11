1433eab595b065e5e846232d16d2e2fb0f689a3d
Justin Lazarski <blackhawks189@gmail.com>
2016-08-10 22:31:52 -0500

    1st commit

---
+++ wave/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  wave
+//
+//  Created by Justin Lazarski on 8/10/16.
+//  Copyright © 2016 Justin Lazarski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
